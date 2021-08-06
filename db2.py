3decfb4e8d837a14aeda13970700648576fe8d02
HongDaYeon <ekdus12037406@gmail.com>
2021-08-06 12:25:50 +0900

    웹크롤링1

---
+++ db2.py
@@ -0,0 +1,33 @@
+#db2.py
+import sqlite3
+
+#연결객체 생성
+con = sqlite3.connect("c:\\work\\sample.db")
+
+#con = sqlite3.connect("c:\\work\\test.db")
+#구문을 수행할 커서 객체를 생성
+cur = con.cursor()
+#데이터를 담을 테이블을 생성
+cur.execute("create table PhoneBook (name text, phoneNum tesxt);")
+#1건을 입력
+cur.execute("insert into PhoneBook values ('derick', '010-111');")
+#입력 파라메터 처리
+name = "gildong"
+phoneNumder = "010-222"
+cur.execute("insert into PhoneBook values (?, ?);", (name,phoneNumder))
+#다중의 레코드(데이터를 입력하는 경우)
+datalist = (("tom","010-123"),("dsp","010-456"))
+cur.executemany("insert into PhoneBook values (?, ?);",datalist)
+
+#결과를 검색
+cur.execute("select * from PhoneBook;")
+print("---fetchone()---")
+print( cur.fetchone() )
+print("---fetchone(2)---")
+print( cur.fetchone(2) )
+print("---fetchall()---")
+cur.execute("select * from PhoneBook;")
+print( cur.fetchall() )
+
+#정상적으로 작업을 완료
+con.commit()
